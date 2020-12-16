4557f964f27df0c550bbb11bacde46ba5957c36d
Lamont Wilson <57460234+LamontWilson@users.noreply.github.com>
2020-12-15 19:53:47 -0600

    Add files via upload
    Added events for mouse moves and clicks and events for color selection

--- myPaint/Form1.cs
+++ myPaint/Form1.cs
@@ -12,9 +12,82 @@ namespace myPaint
 {
     public partial class Form1 : Form
     {
+        //vars
+        Bitmap bmp = new Bitmap(1024, 768);
+        Pen p = new Pen(Color.Black, 5);
+        bool drawing = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //Mouse Events
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing = false;
+            }
+            else
+            {
+                drawing = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawEllipse(p, e.X, e.Y, 3,1);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        //Color Events
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Red;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Orange; 
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Yellow;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Green;
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Blue;
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Indigo;
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Violet;
+        }
+
+        private void toolStripButton8_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Black;
+        }
+
+        private void toolStripButton9_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.White;
         }
     }
 }
